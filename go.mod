28ca47c06bb792268226055e55086331af1cb8a1
zhang <zhang@sss.com>
2024-09-26 01:39:38 +0800

    使用logrus存储日志

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module singo
 
-go 1.20
+go 1.21
+
+toolchain go1.23.0
 
 require (
 	github.com/gavv/httpexpect v1.1.3
@@ -9,7 +11,9 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/validator/v10 v10.14.0
 	github.com/joho/godotenv v1.5.0
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/redis/go-redis/v9 v9.0.5
+	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/crypto v0.22.0
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.5.1
@@ -40,10 +44,12 @@ require (
 	github.com/imkira/go-interpol v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/lestrrat-go/strftime v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -51,10 +57,11 @@ require (
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.33.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
 	github.com/smartystreets/goconvey v1.8.1 // indirect
-	github.com/stretchr/testify v1.8.3 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- util/logger.go
+++ util/logger.go
@@ -2,6 +2,9 @@ package util
 
 import (
 	"fmt"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	log "github.com/sirupsen/logrus"
+
 	"os"
 	"time"
 )
@@ -17,61 +20,66 @@ const (
 	LevelDebug
 )
 
-var logger *Logger
+var logger *Loggers
 
-// Logger 日志
-type Logger struct {
+// Loggers 日志
+type Loggers struct {
 	level int
 }
 
 // Println 打印
-func (ll *Logger) Println(msg string) {
-	fmt.Printf("%s %s", time.Now().Format("2006-01-02 15:04:05 -0700"), msg)
+func (ll *Loggers) Println(msg string) {
+	fmt.Printf("%s %s\n", time.Now().Format("2006-01-02 15:04:05 -0700"), msg)
 }
 
 // Panic 极端错误
-func (ll *Logger) Panic(format string, v ...interface{}) {
+func (ll *Loggers) Panic(format string, v ...interface{}) {
 	if LevelError > ll.level {
 		return
 	}
 	msg := fmt.Sprintf("[Panic] "+format, v...)
+	log.Panic(msg)
 	ll.Println(msg)
 	os.Exit(0)
 }
 
 // Error 错误
-func (ll *Logger) Error(format string, v ...interface{}) {
+func (ll *Loggers) Error(format string, v ...interface{}) {
 	if LevelError > ll.level {
 		return
 	}
 	msg := fmt.Sprintf("[E] "+format, v...)
+	log.Error(msg)
 	ll.Println(msg)
 }
 
 // Warning 警告
-func (ll *Logger) Warning(format string, v ...interface{}) {
+func (ll *Loggers) Warning(format string, v ...interface{}) {
 	if LevelWarning > ll.level {
 		return
 	}
 	msg := fmt.Sprintf("[W] "+format, v...)
+	log.Warning(msg)
 	ll.Println(msg)
 }
 
 // Info 信息
-func (ll *Logger) Info(format string, v ...interface{}) {
+func (ll *Loggers) Info(format string, v ...interface{}) {
 	if LevelInformational > ll.level {
 		return
 	}
 	msg := fmt.Sprintf("[I] "+format, v...)
+	log.Info(msg)
 	ll.Println(msg)
 }
 
 // Debug 校验
-func (ll *Logger) Debug(format string, v ...interface{}) {
+func (ll *Loggers) Debug(format string, v ...interface{}) {
 	if LevelDebug > ll.level {
 		return
 	}
 	msg := fmt.Sprintf("[D] "+format, v...)
+	log.Debug(msg)
 	ll.Println(msg)
 }
 
@@ -87,17 +95,37 @@ func BuildLogger(level string) {
 		intLevel = LevelInformational
 	case "debug":
 		intLevel = LevelDebug
+	default:
+		intLevel = LevelDebug
 	}
-	l := Logger{
+	l := Loggers{
 		level: intLevel,
 	}
 	logger = &l
+	path := "runtime/go.log"
+
+	/* 日志轮转相关函数
+	`WithLinkName` 为最新的日志建立软连接
+	`WithRotationTime` 设置日志分割的时间，隔多久分割一次
+	`WithMaxAge 和 WithRotationCount二者只能设置一个
+	  `WithMaxAge` 设置文件清理前的最长保存时间
+	  `WithRotationCount` 设置文件清理前最多保存的个数
+	*/
+	writer, _ := rotatelogs.New(
+		path+".%Y%m%d",
+		rotatelogs.WithLinkName(path),
+		rotatelogs.WithRotationCount(3),
+		rotatelogs.WithRotationTime(time.Hour*24),
+	)
+
+	log.SetOutput(writer)
+	log.SetFormatter(&log.JSONFormatter{})
 }
 
 // Log 返回日志对象
-func Log() *Logger {
+func Log() *Loggers {
 	if logger == nil {
-		l := Logger{
+		l := Loggers{
 			level: LevelDebug,
 		}
 		logger = &l
